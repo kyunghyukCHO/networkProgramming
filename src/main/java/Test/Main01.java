624cb9bf7b29e701ebcfff4a798dcbca93b99370
kyunghyukcho <rudgur1996@hanmail.net>
2022-09-22 03:11:15 +0900

    update

--- src/main/java/Test/Main01.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class Main01 {
-    public static void main(String[] args) {
-        System.out.println("main01");
-    }
-}
--- src/main/java/Test/Main02.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class Main02 {
-    public static void main(String[] args) {
-        System.out.println("main 02");
-    }
-}
